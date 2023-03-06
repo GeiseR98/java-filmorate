42b93049a37723bb77a7a67c3591e0b1b966e35d
GeiseR98 <ipatovsergejj@yandex.ru>
2023-03-06 21:14:37 +0300

    feat create class UserController and class FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,4 +1,35 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping("/films")
+    public List<Film> findAll() {
+        List<Film> list = new ArrayList<>();
+        for (Integer key : films.keySet()) {
+            list.add(films.get(key));
+        }
+        return list;
+    }
+
+    @PostMapping(value = "/film")
+    public Film addFilm(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+    @PutMapping(value = "/film")
+    public Film changeFilm(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,4 +1,34 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping("/users")
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        for (Integer key : users.keySet()) {
+            list.add(users.get(key));
+        }
+        return list;
+    }
+
+    @PostMapping(value = "/users")
+    public User addFilm(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+    @PutMapping(value = "/users")
+    public User changeFilm(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
 }
