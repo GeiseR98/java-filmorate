194d7624e0f2eaf746652eaeab3736c8389921c1
GeiseR98 <ipatovsergejj@yandex.ru>
2023-03-30 19:19:48 +0300

    набросок "рефакторинга"

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.storage;
+
+public interface FilmStorage {
+    /*
+    для переноса ("рефакторинга") уже есть
+    findAll
+    addFilm
+    changeFilm
+
+    новое
+    removeFilm
+    addLike
+    removeLike
+    getPopularByCount
+    getFilmById
+     */
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.storage;
+
+public interface UserStorage {
+    /*
+    для переноса ("рефакторинга") уже есть
+    findAll
+    addUser
+    changeUser
+
+    новое
+    removeUser
+    addFriend
+    RemoveFriends
+    getUserById
+    getFriends
+     */
+}
