c33423b825aae12fe6d4f6f36ecb45fb99b5124d
GeiseR98 <ipatovsergejj@yandex.ru>
2023-04-02 18:11:38 +0300

    fix: тесты

--- src/test/java/ru/yandex/practicum/filmorate/controllersTest/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllersTest/FilmControllerTest.java
@@ -22,12 +22,7 @@ class FilmControllerTest {
     @BeforeEach
     public void beforeEach() {
         controller = new FilmController(new FilmService(new InMemoryFilmStorage()));
-        controller.addFilm(Film.builder()
-                .name("Don't Be a Menace to South Central While Drinking Your Juice in the Hood")
-                .description("описание слишком долгое, что бы его писать сюда")
-                .releaseDate(LocalDate.parse("1996-01-12", formatter))
-                .duration((long) 89)
-                .build());
+        controller.addFilm(Film.builder().name("Don't Be a Menace to South Central While Drinking Your Juice in the Hood").description("описание слишком долгое, что бы его писать сюда").releaseDate(LocalDate.parse("1996-01-12", formatter)).duration((long) 89).build());
     }
 
     @Test
@@ -37,42 +32,19 @@ class FilmControllerTest {
 
     @Test
     public void shouldNotAddAMovieWithAnIncorrectReleaseDate() {
-        ValidationException ex = assertThrows(
-                ValidationException.class,
-                () -> controller.addFilm(Film.builder()
-                        .name("Man Walking Around A Corner")
-                        .description("Считается самым первым фильмом в мире. Был снят в Париже")
-                        .releaseDate(LocalDate.parse("1887-08-18", formatter))
-                        .duration((long) 1)
-                        .build())
-        );
+        ValidationException ex = assertThrows(ValidationException.class, () -> controller.addFilm(Film.builder().name("Man Walking Around A Corner").description("Считается самым первым фильмом в мире. Был снят в Париже").releaseDate(LocalDate.parse("1887-08-18", formatter)).duration((long) 1).build()));
         assertEquals("дата релиза — не раньше 28 декабря 1895 года", ex.getMessage());
     }
 
     @Test
     public void shouldReplaceTheMovie() {
-        controller.changeFilm(Film.builder()
-                .id(1)
-                .name("Don't Be a Menace to South Central While Drinking Your Juice in the Hood")
-                .description("Фильм о том как черные выживают в гетто")
-                .releaseDate(LocalDate.parse("1996-01-12", formatter))
-                .duration((long) 89)
-                .build());
+        controller.changeFilm(Film.builder().id(1).name("Don't Be a Menace to South Central While Drinking Your Juice in the Hood").description("Фильм о том как черные выживают в гетто").releaseDate(LocalDate.parse("1996-01-12", formatter)).duration((long) 89).build());
         assertEquals("Фильм о том как черные выживают в гетто", controller.findAll().get(0).getDescription());
     }
 
     @Test
     public void shouldGiveAnErrorWhenRequestingANonExistentMovie() {
-        FilmNotFoundException ex = assertThrows(
-                FilmNotFoundException.class,
-                () -> controller.changeFilm(Film.builder()
-                        .id(3)
-                        .name("Don't Be a Menace to South Central While Drinking Your Juice in the Hood")
-                        .description("Фильм о том как черные выживают в гетто")
-                        .releaseDate(LocalDate.parse("1996-01-12", formatter))
-                        .duration((long) 89)
-                        .build())
-        );
+        FilmNotFoundException ex = assertThrows(FilmNotFoundException.class, () -> controller.changeFilm(Film.builder().id(3).name("Don't Be a Menace to South Central While Drinking Your Juice in the Hood").description("Фильм о том как черные выживают в гетто").releaseDate(LocalDate.parse("1996-01-12", formatter)).duration((long) 89).build()));
         assertEquals(String.format("Фильм с номером %s не найден", 3), ex.getMessage());
     }
 }
