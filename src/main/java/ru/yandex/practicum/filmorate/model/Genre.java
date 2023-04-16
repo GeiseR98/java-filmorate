a5da6582626d840b3a396f15ca495b7da9824680
GeiseR98 <ipatovsergejj@yandex.ru>
2023-04-16 06:38:25 +0300

    feat Genre and Mpa

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+public class Genre {
+    private int genre_id;
+    @NotBlank(message = "название не может быть пустым")
+    private final String genre_name;
+}
