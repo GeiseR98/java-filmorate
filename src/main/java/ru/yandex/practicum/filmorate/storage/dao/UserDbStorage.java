3b9184beb148141c53e8bdabc5953b4bd3f22b44
GeiseR98 <ipatovsergejj@yandex.ru>
2023-04-16 21:22:14 +0300

    feat UserDbStorage (осталось развозиться с друзьями)

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.storage.dao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 import ru.yandex.practicum.filmorate.storage.mappers.UserMapper;
@@ -16,6 +18,7 @@ import java.util.List;
 
 @Repository
 @Primary
+@Slf4j
 public class UserDbStorage implements UserStorage {
     private final JdbcTemplate jdbcTemplate;
     private final UserMapper userMapper;
@@ -28,6 +31,12 @@ public class UserDbStorage implements UserStorage {
     public List<User> getAllUsers() {
         String sqlQuery = "SELECT * FROM users";
         return jdbcTemplate.query(sqlQuery, userMapper);
+    }
+
+    @Override
+    public User getUserById(Integer id) {
+        String sqlQuery = "SELECT * FROM users WHERE user_id = ?";
+        return jdbcTemplate.queryForObject(sqlQuery, userMapper, id);
     }
 
     @Override
@@ -54,7 +63,7 @@ public class UserDbStorage implements UserStorage {
     public User changeUser(User user) {
         String sqlQuery = "UPDATE users SET " +
                 "name = ?, email = ?, login = ?, birthday = ? " +
-                "WERE user_id = ?";
+                "WHERE user_id = ?";
         jdbcTemplate.update(sqlQuery,
                 user.getName(),
                 user.getEmail(),
@@ -65,37 +74,53 @@ public class UserDbStorage implements UserStorage {
     }
 
     @Override
-    public boolean isUserPresent(Integer id) {
-        return false;
-    }
-
-    @Override
     public void removeUsers(Integer id) {
-
+        String sqlQuery = "DELETE FROM users WHERE user_id = ?";
+        jdbcTemplate.update(sqlQuery, id);
     }
 
     @Override
-    public User getUserById(Integer id) {
-        String sqlQuery = "SELECT * FROM users WHERE user_id = ?";
-        return jdbcTemplate.queryForObject(sqlQuery, userMapper, id);
+    public void addAsFriends(Integer oneId, Integer twoId) { // добавить проверку и ветвление
+        isUserPresent(oneId);
+        isUserPresent(twoId);
+        String sqlQuery = "INSERT INTO friends (friend_one_id, friend_two_id, status) " +
+                "VALUES (?, ?, ?)";
+        jdbcTemplate.update(sqlQuery, oneId, twoId, false);
     }
 
     @Override
-    public void addAsFriends(Integer oneId, Integer twoId) {
+    public List<User> getFriends(Integer id) { // запрос вернуть друзей переделать
+        String sqlQuery = "SELECT * FROM users " +
+                "WHERE user_id IN (" +
+                "SELECT friend_two_id " +
+                "FROM friends WHERE friend_one_id = ?)";
+        return jdbcTemplate.query(sqlQuery, userMapper, id);
+    }
 
+    @Override
+    public List<User> getListOfMutualFriends(Integer oneId, Integer twoId) {
+        String sqlQuery = "SELECT * FROM users " +
+                "JOIN (SELECT friend_two_id FROM friends WHERE friend_one_id = ?) AS fr " +
+                "ON users.friend_one_id = fr.friend_two_id " +
+                "JOIN (SELECT friend_two_id FROM friends WHERE friend_one_id = ?) AS nfr " +
+                "ON user.friend_one_id = nfr.friend_two_id";
+        return jdbcTemplate.query(sqlQuery, userMapper, oneId, twoId);
+    }
+
+    @Override
+    public boolean isUserPresent(Integer id) {
+        String query = "SELECT COUNT (*) FROM users WHERE user_id = ?";
+        if (jdbcTemplate.queryForObject(query, Integer.class, id) == 0) {
+            log.debug(String.format("Пользователь с идентификатором %s не найден", id));
+            throw new UserNotFoundException(String.format("Пользователь с идентификатором %s не найден", id));
+        }
+        return true;
     }
 
     @Override
     public void removeFromFriends(Integer oneId, Integer twoId) {
+        String sqlQuery = "DELETE FROM friends WHERE friend_one_id = ? AND friend_two_id = ?";
+        jdbcTemplate.update(sqlQuery, oneId, twoId);
+    }
 
-    }
-    @Override
-    public List<User> getFriends(Integer id) {
-        String sqlQuery = "SELECT * FROM users AS u " +
-                "JOIN (SELECT friend_two_id " +
-                "FROM friends " +
-                "WHERE friend_one_id = ?) AS f " +
-                "ON u.friend_one_id = f.friend_two_id";
-        return jdbcTemplate.query(sqlQuery, userMapper, id);
-    }
 }
