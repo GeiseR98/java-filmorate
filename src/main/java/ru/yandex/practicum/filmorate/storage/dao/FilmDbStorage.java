712420c0b1221d0d8e240cfb385e4acb065454b5
GeiseR98 <ipatovsergejj@yandex.ru>
2023-04-17 05:27:04 +0300

    feat: film the end

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorage.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.dao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
@@ -7,9 +8,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.mappers.FilmMapper;
 
@@ -20,6 +22,7 @@ import java.util.*;
 
 @Repository
 @Primary
+@Slf4j
 public class FilmDbStorage implements FilmStorage {
 
     private final JdbcTemplate jdbcTemplate;
@@ -106,36 +109,78 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Film changeFilm(Film film) {
-        return null;
+        String sqlQuery = "UPDATE films SET " +
+                "name = ?, description = ?, release_date = ?, duration = ?, mpa_id = ? " +
+                "WHERE film_id = ?";
+        jdbcTemplate.update(sqlQuery,
+                film.getName(),
+                film.getDescription(),
+                Date.valueOf(film.getReleaseDate()),
+                film.getDuration(),
+                film.getMpa().getId(),
+                film.getId());
+        addGenre(film);
+        return getFilmById(film.getId());
     }
 
     @Override
     public void removeFilmById(Integer id) {
-
+        String sqlQuery = "DELETE FROM films WHERE films_id = ?";
+        jdbcTemplate.update(sqlQuery, id);
     }
 
     @Override
     public void addLike(Integer filmId, Integer userId) {
-
+        String sqlQuery = "INSERT INTO likes (film_id, user_id) " +
+                "VALUES (?, ?)";
+        jdbcTemplate.update(sqlQuery, filmId, userId);
     }
 
     @Override
     public void removeLike(Integer filmId, Integer userId) {
-
+        String sqlQuery = "DELETE FROM likes " +
+                "WHERE film_id = ? AND user_id = ?";
+        jdbcTemplate.update(sqlQuery, filmId, userId);
     }
 
     @Override
     public List<Film> getPopularFilms(Integer count) {
-        return null;
+        String sqlQuery = "SELECT f.*, m.mpa_name, g.genre_id, g.genre_name FROM films AS f " +
+                "LEFT JOIN (SELECT film_id, COUNT(user_id) AS likes " +
+                "FROM likes GROUP BY film_id) AS l ON f.film_id = l.film_id " +
+                "LEFT JOIN mpa AS m ON f.mpa_id = m.mpa_id " +
+                "LEFT JOIN film_genre AS fg ON f.film_id = fg.film_id " +
+                "LEFT JOIN genre AS g ON fg.genre_id = g.genre_id " +
+                "ORDER BY likes DESC " +
+                "LIMIT " + count;
+
+        return outputtingListFilm(sqlQuery);
     }
 
     @Override
     public Film getFilmById(Integer id) {
-        return null;
+        String sqlQuery = "SELECT f.*, m.mpa_name, g.genre_id, g.genre_name " +
+                "FROM films AS f JOIN MPA AS m ON f.mpa_id = m.mpa_id " +
+                "LEFT JOIN film_genre AS f_g ON f.film_id = f_g.film_id " +
+                "LEFT JOIN genre AS g ON f_g.genre_id = g.genre_id " +
+                "WHERE f.film_id = " + id;
+
+        List<Film> films = outputtingListFilm(sqlQuery);
+        if (!films.isEmpty()) {
+            return films.get(0);
+        } else {
+            log.debug("Фильм с номером {} не найден", id);
+            throw new FilmNotFoundException(String.format("Фильм с номером %s не найден", id));
+        }
     }
 
     @Override
     public boolean isFilmPresent(Integer id) {
-        return false;
+        String query = "SELECT COUNT (*) FROM films WHERE film_id = ?";
+        if (jdbcTemplate.queryForObject(query, Integer.class, id) == 0) {
+            log.debug(String.format("Фильм с номером %s не найден", id));
+            throw new UserNotFoundException(String.format("Фильм с номером %s не найден", id));
+        }
+        return true;
     }
 }
